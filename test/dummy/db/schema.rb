00bda3e10c3fda71038fae08243da03225ddd503
Jake Hurd <jake.hurd@gmail.com>
2020-09-01 22:19:35 -0500

    Oops take 2

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,12 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_08_31_144824) do
-
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "status", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
 end
