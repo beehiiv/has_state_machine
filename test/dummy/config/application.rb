87eeb555228da2f8b8becc0b57d6a193ee71733f
Benjamin Hargett <hargettbenjamin@gmail.com>
2020-09-03 16:02:44 -0400

    silence warning and load default config for each version

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -9,5 +9,10 @@ require "transitioner"
 
 module Dummy
   class Application < Rails::Application
+    config.load_defaults "#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}".to_f
+
+    if Rails::VERSION::MAJOR == 5
+      config.active_record.sqlite3.represent_boolean_as_integer = true
+    end
   end
 end
